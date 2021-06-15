c60054487583beca980cd7c8e1f12a400c75fea9
Inrix <Inrixia@gmail.com>
2021-06-16 11:57:01 +1200

    Updated to TS

---
+++ src/index.ts
@@ -0,0 +1,57 @@
+import Imap, { Config } from "imap";
+import { simpleParser } from "mailparser";
+
+type ImapOptions = { 
+	mailbox?: string, 
+	markSeen?: boolean 
+}
+
+export class ImapListener extends Imap {
+	public mailbox: string;
+	public markSeen: boolean;
+	/**
+	 * Listens for new emails, emits "mail" and "err" events.
+	 * @param config
+	 */
+	constructor(config: Config, options?: ImapOptions) {
+		super(config);
+		this.mailbox = options?.mailbox || "INBOX";
+		this.markSeen = options?.markSeen || true;
+		this.on("mail", this.fetchUnseen);
+	}
+	start = () => new Promise<void>((res, rej) => {
+		this.once("ready", () => {
+			this.openBox(this.mailbox, false, (err) => {
+				if (err) rej(err);
+				else res();
+			});
+		});
+		this.connect();
+	});
+	stop = () => new Promise(res => {
+		this.on("close", res);
+		this.end();
+	});
+
+	fetchUnseen = () => {
+		this.search(["UNSEEN"], (err, seachResults) => {
+			if (err) return this.emit("error", err);
+
+			if (!seachResults || seachResults.length === 0) return;
+
+			const fetch = this.fetch(seachResults, {
+				markSeen: this.markSeen,
+				bodies: "",
+			});
+			fetch.on("message", (msg) => {
+				msg.once("body", (stream) => {
+					simpleParser(stream, (err, mail) => {
+						if (err) this.emit("error", err);
+						else this.emit("email", mail);
+					});
+				});
+			});
+			fetch.once("error", (err) => this.emit("error", err));
+		});
+	};
+}
